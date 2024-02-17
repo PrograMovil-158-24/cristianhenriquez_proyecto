bcb417ea7da128f523e20af91b40e96ff04f1f59
Cristian Henriquez <c.henriquez_94@hotmail.com>
2024-02-16 20:59:56 -0600

    retornar items

--- cristianhenriquez_proyecto/lib/widgets/items_libros.dart
+++ cristianhenriquez_proyecto/lib/widgets/items_libros.dart
@@ -4,15 +4,15 @@ import 'package:flutter/material.dart';
 
 List<Widget> listItems(List<dynamic>? data, BuildContext context) {
   List<Widget> listItems = [];
-data?.forEach((element) {
-  listItems.add(
-    ListTile(
+  data?.forEach((element) {
+    listItems.add(ListTile(
       title: Text(element.name),
-        subtitle: Text(element.title),
-        onTap: () {
-          print('Tapped');
-          //Navegacion a una pantalle del equipo .....  equipo/:id
-        },
-    )
-  )
-})
+      subtitle: Text(element.title),
+      onTap: () {
+        // print('Tapped');
+        //Navegacion a una pantalle del equipo .....  equipo/:id
+      },
+    ));
+  });
+  return listItems;
+}
